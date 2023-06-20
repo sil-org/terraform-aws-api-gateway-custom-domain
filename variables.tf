608e43f3c937bab8d8767a07c79cdc15aab2d3d9
Matt H <matt_henderson@sil.org>
2023-06-20 13:42:27 -0400

    Add the main, outputs, and variables files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+
+variable "api_name" {
+  description = "The API Gateway API name, for associating with this custom domain"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "The ARN for the Amazon certificate to use for this domain name"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "The exact domain name for the API Gateway custom domain"
+  type        = string
+}
